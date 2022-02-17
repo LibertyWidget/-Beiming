6e942a1dcac64c0f4126e56ce2862d0b9e0baeb2
Patrick <jinshakx@gmail.com>
2022-02-17 23:10:04 +0800

    init

---
+++ Beiming/app/Mian.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Beiming.app
+{
+    /// <summary>
+    /// Mian.xaml 的交互逻辑
+    /// </summary>
+    public partial class Mian : Window
+    {
+        public Mian()
+        {
+            InitializeComponent();
+        }
+    }
+}
